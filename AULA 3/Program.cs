ab4c153c59a6102d02343f763e26697610c6ff32
felipe.msilveira1 <felipe.msilveira1@CAS554856A107D>
2023-02-27 19:58:44 -0300

    Adicionar arquivos de projeto.

---
+++ AULA 3/Program.cs
@@ -0,0 +1,45 @@
+﻿//namespace AULA_3
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            List<int> list = new List<int>();
+//            list.Add(30);
+//            list.Add(10);
+//            list.Add(30);
+//            list.Add(1);
+
+//            list.Sort();
+
+
+//            foreach (int i in list)
+//            {
+//                Console.WriteLine(i);
+//            }
+//        }
+//    }
+//}  //lista ORDENADA
+
+//namespace AULA_3
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            List<int> list = new List<int>();
+//            list.Add(30);
+//            list.Add(10);
+//            list.Add(130);
+//            list.Add(1);
+//            list.Add(200);
+
+//            List<int> valores = list.FindAll(item => item > 100);
+//            foreach (int i in valores)
+//            {
+//                Console.WriteLine(i);
+//            }
+//        }
+//    }
+//}
+
